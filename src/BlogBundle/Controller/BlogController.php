e1b6facfd28d9d02cb1fd855ceb099913b5a9df2
vagrant <vagrant@homestead>
2017-05-22 11:58:33 +0000

    add index and show actions in BlogController

--- src/BlogBundle/Controller/BlogController.php
+++ src/BlogBundle/Controller/BlogController.php
@@ -4,15 +4,27 @@ namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class BlogController extends Controller
 {
+
     /**
-     * @Route("/")
+     * @Route("/{page}", defaults={"page" = 1})
      */
-    public function indexAction()
+    public function indexAction($page)
     {
-        return $this->render('BlogBundle:Blog:index.html.twig');
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $paginator = $entityManager->getRepository('BlogBundle:Post')->getLatestPosts($page);
+
+        $posts = $paginator->getIterator();
+
+        if (count($posts) == 0) {
+            throw new NotFoundHttpException('404. Page not found');
+        }
+
+        return $this->render('BlogBundle:Blog:index.html.twig', compact('posts', $paginator));
     }
 
     /**
@@ -20,6 +32,14 @@ class BlogController extends Controller
      */
     public function showAction($id)
     {
-        return $this->render('BlogBundle:Blog:show.html.twig');
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $post = $entityManager->getRepository('BlogBundle:Post')->find($id);
+
+        if (is_null($post)) {
+            throw new NotFoundHttpException('404. Page not found');
+        }
+
+        return $this->render('BlogBundle:Blog:show.html.twig', compact('post'));
     }
 }
